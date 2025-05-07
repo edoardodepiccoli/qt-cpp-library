6017d34c9fa577074accf731a4c58c99b6c579b2
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-07 22:45:02 +0200

    Implement NewItemView with item type selection and dynamic form setup
    - Added a QComboBox for selecting item types (Book, Movie, Article).
    - Integrated a QScrollArea to allow for better layout management.
    - Implemented setUpForm method to dynamically create and display forms based on the selected item type.
    - Connected type selection changes to update the displayed form accordingly.

--- app/views/newitemview.cpp
+++ app/views/newitemview.cpp
@@ -1,13 +1,79 @@
 #include "newitemview.h"
+#include "../visitors/itemformvisitor.h"
+
 #include <QVBoxLayout>
 #include <QLabel>
-
+#include <QComboBox>
+#include <QScrollArea>
 NewItemView::NewItemView(QWidget *parent)
     : QWidget(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(new QLabel("This is NewItemView"));
-    layout->addStretch();
+    QScrollArea *scrollArea = new QScrollArea(this);
+    scrollArea->setWidgetResizable(true);
 
-    setLayout(layout);
+    QWidget *contentWidget = new QWidget;
+    mainLayout = new QVBoxLayout(contentWidget);
+
+    // Add type selection combo box
+    typeComboBox = new QComboBox(this);
+    typeComboBox->addItem("book", "book");
+    typeComboBox->addItem("movie", "movie");
+    typeComboBox->addItem("article", "article");
+
+    mainLayout->addWidget(new QLabel("Select Item Type:"));
+    mainLayout->addWidget(typeComboBox);
+    mainLayout->addStretch();
+
+    connect(typeComboBox, &QComboBox::currentTextChanged,
+            this, &NewItemView::onTypeChanged);
+
+    setUpForm("book");
+
+    contentWidget->setLayout(mainLayout);
+    scrollArea->setWidget(contentWidget);
+
+    QVBoxLayout *outerLayout = new QVBoxLayout(this);
+    outerLayout->addWidget(scrollArea);
+    setLayout(outerLayout);
+}
+
+void NewItemView::setUpForm(const QString &type)
+{
+    qDebug() << "Setting up form";
+
+    if (currentForm)
+    {
+        mainLayout->removeWidget(currentForm);
+        qDebug() << "Removing previous form";
+        delete currentForm;
+        currentForm = nullptr;
+    }
+
+    ItemFormVisitor *visitor = new ItemFormVisitor(this);
+
+    if (type == "book")
+    {
+        Book *book = new Book();
+        visitor->visit(*book);
+    }
+    else if (type == "movie")
+    {
+        Movie *movie = new Movie();
+        visitor->visit(*movie);
+    }
+    else if (type == "article")
+    {
+        Article *article = new Article();
+        visitor->visit(*article);
+    }
+
+    QWidget *form = visitor->getResult();
+    mainLayout->addWidget(form);
+    currentForm = form;
+}
+
+void NewItemView::onTypeChanged(const QString &type)
+{
+    qDebug() << "Selected item type:" << type;
+    setUpForm(type);
 }
--- app/views/newitemview.h
+++ app/views/newitemview.h
@@ -1,9 +1,21 @@
 #pragma once
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QComboBox>
 
 class NewItemView : public QWidget
 {
     Q_OBJECT
 public:
     explicit NewItemView(QWidget *parent = nullptr);
+
+    void setUpForm(const QString &type);
+
+private:
+    QVBoxLayout *mainLayout;
+    QComboBox *typeComboBox;
+    QWidget *currentForm;
+
+private slots:
+    void onTypeChanged(const QString &type);
 };
