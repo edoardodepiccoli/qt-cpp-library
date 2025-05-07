d93862d5a5393b666f358fce51d39eeee8b07f66
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-07 22:42:56 +0200

    format jsonvisitor header file with vscode c++ extension

--- app/visitors/jsonvisitor.h
+++ app/visitors/jsonvisitor.h
@@ -7,16 +7,17 @@
 #include "../models/article.h"
 #include <QJsonObject>
 
-class JSONVisitor : public Visitor {
+class JSONVisitor : public Visitor
+{
 public:
     QJsonObject getResult() const { return result; }
-    
-    void visit(Book& book) override;
-    void visit(Movie& movie) override;
-    void visit(Article& article) override;
+
+    void visit(Book &book) override;
+    void visit(Movie &movie) override;
+    void visit(Article &article) override;
 
 private:
     QJsonObject result;
 };
 
-#endif // JSONVISITOR_H 
+#endif // JSONVISITOR_H
