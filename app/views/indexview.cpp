cc1dcbe90c68723ccbeffa204302418541ae4d68
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-07 13:15:55 +0200

    update indexview to display all items in a list format

--- app/views/indexview.cpp
+++ app/views/indexview.cpp
@@ -1,13 +1,52 @@
 #include "indexview.h"
+#include "../models/library.h"
+#include "../visitors/widgetvisitor.h"
+
 #include <QVBoxLayout>
-#include <QLabel>
+#include <QScrollArea>
 
 IndexView::IndexView(QWidget *parent)
     : QWidget(parent)
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(new QLabel("This is IndexView"));
-    layout->addStretch();
+    // Scroll area to wrap content
+    QScrollArea *scrollArea = new QScrollArea(this);
+    scrollArea->setWidgetResizable(true);
 
-    setLayout(layout);
+    // Content widget that will hold all item widgets
+    QWidget *contentWidget = new QWidget;
+    mainLayout = new QVBoxLayout(contentWidget);
+    mainLayout->setAlignment(Qt::AlignTop); // Stack items vertically
+    mainLayout->setSpacing(12);
+
+    contentWidget->setLayout(mainLayout);
+    scrollArea->setWidget(contentWidget);
+
+    // Final layout of IndexView
+    QVBoxLayout *outerLayout = new QVBoxLayout(this);
+    outerLayout->addWidget(scrollArea);
+    setLayout(outerLayout);
+}
+
+void IndexView::populateFromLibrary(Library *library)
+{
+    // Clear existing cards
+    QLayoutItem *item;
+    while ((item = mainLayout->takeAt(0)) != nullptr)
+    {
+        delete item->widget();
+        delete item;
+    }
+
+    // Generate a card for each item
+    for (int i = 0; i < library->getItemCount(); ++i)
+    {
+        Item *item = library->getItem(i);
+        WidgetVisitor visitor;
+        item->accept(visitor);
+        if (QWidget *w = visitor.getResult())
+        {
+            w->setStyleSheet("background-color: gray;");
+            mainLayout->addWidget(w);
+        }
+    }
 }
--- app/views/indexview.h
+++ app/views/indexview.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <QWidget>
 
+class QVBoxLayout;
+class Library;
+
 class IndexView : public QWidget
 {
     Q_OBJECT
 public:
     explicit IndexView(QWidget *parent = nullptr);
+    void populateFromLibrary(Library *library);
+
+private:
+    QVBoxLayout *mainLayout;
 };
