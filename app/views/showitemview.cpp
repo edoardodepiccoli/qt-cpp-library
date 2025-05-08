eb9959bee53d514d3ae078ed30c532d0f624a680
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-08 12:34:27 +0200

    Refactor ShowItemView to improve item management and visitor usage
    - Updated ShowItemView constructor to store a reference to the item instead of taking ownership, enhancing memory management.
    - Changed visitor instantiation to use std::make_unique for better resource management.
    - Adjusted signal connections to use the visitor's raw pointer, ensuring proper handling of item requests.

--- app/views/showitemview.cpp
+++ app/views/showitemview.cpp
@@ -5,15 +5,11 @@
 #include "showitemview.h"
 #include "../visitors/itemshowvisitor.h"
 
-ShowItemView::ShowItemView(QWidget *parent, Item *item) : QWidget(parent)
+ShowItemView::ShowItemView(QWidget *parent, Item *item) : QWidget(parent), item(item)
 {
     qDebug() << "ShowItemView constructor";
-    if (item)
-    {
-        this->item = std::unique_ptr<Item>(item); // Take ownership
-    }
 
-    ItemShowVisitor *visitor = new ItemShowVisitor(this);
+    visitor = std::make_unique<ItemShowVisitor>(this);
     if (this->item)
     {
         this->item->accept(*visitor);
@@ -22,12 +18,10 @@ ShowItemView::ShowItemView(QWidget *parent, Item *item) : QWidget(parent)
     QVBoxLayout *layout = new QVBoxLayout(this);
     QWidget *result = visitor->getResult();
 
-    connect(visitor, &ItemShowVisitor::deleteItemRequested, this, &ShowItemView::deleteItemRequested);
-    connect(visitor, &ItemShowVisitor::editItemRequested, this, &ShowItemView::editItemRequested);
+    connect(visitor.get(), &ItemShowVisitor::deleteItemRequested, this, &ShowItemView::deleteItemRequested);
+    connect(visitor.get(), &ItemShowVisitor::editItemRequested, this, &ShowItemView::editItemRequested);
 
     layout->addWidget(result);
 
     this->setLayout(layout);
 }
-
-ShowItemView::~ShowItemView() = default;
--- app/views/showitemview.h
+++ app/views/showitemview.h
@@ -3,20 +3,22 @@
 #include <memory>
 
 #include "../models/item.h"
+#include "../visitors/itemshowvisitor.h"
 
 class ShowItemView : public QWidget
 {
     Q_OBJECT
 
 public:
-    ShowItemView(QWidget *parent = nullptr, Item *item = nullptr);
-    ~ShowItemView();
+    explicit ShowItemView(QWidget *parent, Item *item);
+    ~ShowItemView() = default;
 
 signals:
-    void deleteItemRequested(const QUuid &itemId);
-    void editItemRequested(const QUuid &itemId);
+    void deleteItemRequested(const QUuid &id);
+    void editItemRequested(const QUuid &id);
 
 private:
-    std::unique_ptr<Item> item; // Take ownership of the item
+    Item *item; // Store a reference instead of taking ownership
     QVBoxLayout *layout;
+    std::unique_ptr<ItemShowVisitor> visitor; // Own the visitor
 };
