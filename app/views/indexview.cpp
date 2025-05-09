bf720e8a65612d17061cc98e160f0c3176e21b32
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-09 09:52:08 +0200

    Add search functionality to IndexView
    - Introduced a QLineEdit search bar for user input, allowing dynamic searching of items.
    - Updated populateFromLibrary method to accept a search query and filter results accordingly.
    - Implemented onSearchTextChanged slot to handle search input changes and refresh displayed items.
    - Ensured null library checks to prevent crashes during search operations.

--- app/views/indexview.cpp
+++ app/views/indexview.cpp
@@ -6,10 +6,16 @@
 #include <QScrollArea>
 #include <QUuid>
 #include <QDebug>
+#include <QLineEdit>
 
 IndexView::IndexView(QWidget *parent)
     : QWidget(parent), libraryModel(nullptr)
 {
+    QLineEdit *searchBar = new QLineEdit(this);
+    searchBar->setPlaceholderText("Search...");
+    searchBar->setFixedHeight(30);
+    connect(searchBar, &QLineEdit::textChanged, this, &IndexView::onSearchTextChanged);
+
     QScrollArea *scrollArea = new QScrollArea(this);
     scrollArea->setWidgetResizable(true);
 
@@ -22,6 +28,8 @@ IndexView::IndexView(QWidget *parent)
     scrollArea->setWidget(contentWidget);
 
     QVBoxLayout *outerLayout = new QVBoxLayout(this);
+    outerLayout->setSpacing(0);
+    outerLayout->addWidget(searchBar);
     outerLayout->addWidget(scrollArea);
     setLayout(outerLayout);
 }
@@ -33,9 +41,13 @@ IndexView::~IndexView()
     visitors.clear();
 }
 
-void IndexView::populateFromLibrary(Library *library)
+void IndexView::populateFromLibrary(Library *library, QString &searchQuery)
 {
+    if (!library)
+        return; // Guard against null library
+
     libraryModel = library;
+    currentSearchQuery = searchQuery;
 
     // Clear existing cards and visitors
     QLayoutItem *item;
@@ -47,31 +59,42 @@ void IndexView::populateFromLibrary(Library *library)
     qDeleteAll(visitors);
     visitors.clear();
 
-    // Generate a card for each item
-    for (int i = 0; i < libraryModel->getItemCount(); ++i)
-    {
-        Item *item = libraryModel->getItem(i);
-        if (item)
-        {
-            // Pass this (IndexView) as the parent to the visitor
-            ItemCardVisitor *visitor = new ItemCardVisitor(this);
-            visitors.append(visitor); // Track the visitor
-            item->accept(*visitor);
-            if (QWidget *w = visitor->getResult())
-            {
-                w->setParent(this); // Explicitly set parent
-                w->setStyleSheet("background-color: gray;");
-                mainLayout->addWidget(w);
+    // Get search results
+    std::vector<Item *> searchResults = libraryModel->searchItems(currentSearchQuery);
 
-                connect(visitor, &ItemCardVisitor::itemShowRequested,
-                        this, &IndexView::itemShowRequested);
-            }
-            else
-            {
-                // If the visitor didn't create a widget, delete the visitor
-                visitors.removeOne(visitor);
-                delete visitor;
-            }
+    // Generate a card for each search result
+    for (Item *item : searchResults)
+    {
+        if (!item)
+            continue; // Skip null items
+
+        // Pass this (IndexView) as the parent to the visitor
+        ItemCardVisitor *visitor = new ItemCardVisitor(this);
+        visitors.append(visitor); // Track the visitor
+        item->accept(*visitor);
+        if (QWidget *w = visitor->getResult())
+        {
+            w->setParent(this); // Explicitly set parent
+            w->setStyleSheet("background-color: gray;");
+            mainLayout->addWidget(w);
+
+            connect(visitor, &ItemCardVisitor::itemShowRequested,
+                    this, &IndexView::itemShowRequested);
         }
+        else
+        {
+            // If the visitor didn't create a widget, delete the visitor
+            visitors.removeOne(visitor);
+            delete visitor;
+        }
+    }
+}
+
+void IndexView::onSearchTextChanged(const QString &query)
+{
+    if (libraryModel)
+    {
+        currentSearchQuery = query; // Update the current search query
+        populateFromLibrary(libraryModel, currentSearchQuery);
     }
 }
--- app/views/indexview.h
+++ app/views/indexview.h
@@ -15,14 +15,21 @@ public:
     explicit IndexView(QWidget *parent = nullptr);
     ~IndexView() override;
 
-    void populateFromLibrary(Library *library);
+    void populateFromLibrary(Library *library, QString &searchQuery);
 
 signals:
     // Qt automatically forwards the parameters when the signatures match, cool stuff
     void itemShowRequested(const QUuid &itemId);
 
+private slots:
+    void onSearchTextChanged(const QString &query);
+
 private:
     QVBoxLayout *mainLayout;
+
+    // These two go hand in hand, maybe they are too coupled together but for now it's fine
     Library *libraryModel;
+    QString currentSearchQuery;
+
     QList<ItemCardVisitor *> visitors;
 };
